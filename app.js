49299a0885ed8e53e040bfdc02a44483db884bc2
marccdb <marcio.bianco@gmail.com>
2020-04-19 20:50:12 -0300

    front end tweak,, initial work on back end

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = express();
 
 app.use(express.static("public"));
 
+app.use(bodyParser.urlencoded({ extended: true }))
+
 app.listen(3000, function(){console.log("Listening on port 3000")});
 
 app.get("/", function(req, res){
@@ -11,6 +13,7 @@ app.get("/", function(req, res){
 });
 
 app.post("/", function(req, res){
+    var campoBusca = req.body.campoBusca;
 
-
+    console.log(campoBusca);
 });
