6a3c94c0cf683354373d33458aa6e27ab20abe8f
marccdb <marcio.bianco@gmail.com>
2020-04-19 17:49:42 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(express.static("public"));
+
+app.listen(3000, function(){console.log("Listening on port 3000")});
+
+app.get("/", function(req, res){
+    res.sendFile(__dirname + "/index.html")
+});
+
+app.post("/", function(req, res){
+
+
+});
