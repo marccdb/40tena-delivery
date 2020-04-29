89d8d7ac140e90791ebc7051a046c1f4cc7f98c3
marccdb <marcio.bianco@gmail.com>
2020-04-29 15:43:12 -0300

    introduce SPA, initial work on suppliers

--- public/index.js
+++ public/index.js
@@ -14,3 +14,20 @@ function toggleVisibility() {
 botaoCadastro.addEventListener("click", toggleVisibility);
 botaoVoltar.addEventListener("click", toggleVisibility);
 
+
+//VUE
+
+const fornecedoresArray = [];
+
+const app = new Vue({
+    el: '#fornecedores',
+    data: {
+        name: 'NOME FORNECEDOR',
+        ramo: "alim",
+        cidade: "Cidade",
+        telefone: "Telefone",
+        infoFornecedor: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin malesuada ornare diam. Praesent suscipit diam id commodo tincidunt. Maecenas vel nibh orci. Interdum et malesuada fames ac ante ipsum primis in faucibus.",
+        imgAlim: "https://image.flaticon.com/icons/svg/2871/2871612.svg",
+        imgServ: "https://image.flaticon.com/icons/png/512/2304/2304375.png"
+    }
+})
