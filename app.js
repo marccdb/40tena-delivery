fbbd30a53344278d8692e14fe72b17b3d400fdd6
marccdb <marcio.bianco@gmail.com>
2020-05-08 17:16:01 -0300

    code refactoring

--- app.js
+++ app.js
@@ -1,77 +1,84 @@
 //Requiring dependencies
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 //Dependencies settings
-app.use(express.static("public"));
+app.use(express.static('public'));
 const schema = mongoose.Schema;
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
 //Opening server
 app.listen(process.env.PORT || 3000, function () {
-    console.log("Listening on port 3000");
+  console.log('Listening on port 3000');
 });
-
 
 //Mongoose (MongoDB) Set up
 
 function startServer() {
-    mongoose.connect('mongodb+srv://marcio:WfPtgB1NNVVENg9l@cluster0-rtrkq.gcp.mongodb.net/test?retryWrites=true&w=majority', {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    });
+  mongoose.connect(
+    'mongodb+srv://marcio:WfPtgB1NNVVENg9l@cluster0-rtrkq.gcp.mongodb.net/40tena-delivery?retryWrites=true&w=majority',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    },
+    (err) => {
+      if (err) console.log(err);
+      else console.log('MongoDB connection success');
+    }
+  );
 }
 
 const fornecedorSchema = new schema({
-    name: String,
-    ramo: String,
-    cidade: String,
-    telefone: Number,
-    descricao: String
+  name: String,
+  ramo: String,
+  cidade: String,
+  telefone: Number,
+  descricao: String,
 });
 
-const fornecedor = mongoose.model('Fornecedor', fornecedorSchema);
+const fornecedor = mongoose.model('fornecedores', fornecedorSchema);
 
-
-
-
-
+const fornecedoresArr = [];
 
 //ROUTES
-app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/index.html");
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
 });
 
+app.post('/', function (req, res) {
+  let campoBusca = req.body.campoBusca;
 
-app.post("/", function (req, res) {
-    let campoBusca = req.body.campoBusca;
-
-    console.log(campoBusca); //retirar em versão final
+  console.log(campoBusca); //retirar em versão final
 });
 
-app.post("/cadastro", function (req, res) {
+app.post('/cadastro', function (req, res) {
+  let getName = req.body.formName;
+  let getCategory = req.body.formCategory;
+  let getCity = req.body.formCity;
+  let getTelephone = req.body.formTelephone;
+  let getDescription = req.body.formDescription;
 
-    let getName = req.body.formName;
-    let getCategory = req.body.formCategory;
-    let getCity = req.body.formCity;
-    let getTelephone = req.body.formTelephone;
-    let getDescription = req.body.formDescription;
+  startServer();
 
-    startServer();
+  const newFornecedor = new fornecedor({
+    name: getName,
+    ramo: getCategory,
+    cidade: getCity,
+    telefone: getTelephone,
+    descricao: getDescription,
+  });
 
-    const newFornecedor = new fornecedor({
-        name: getName,
-        ramo: getCategory,
-        cidade: getCity,
-        telefone: getTelephone,
-        descricao: getDescription
-    });
+  newFornecedor.save((err) => {
+    if (err) console.log(err);
+  });
+  fornecedoresArr.push(newFornecedor);
+  //mongoose.connection.close( err => {if (err) console.log(err); else console.log("MongoDB connection closed")});
 
-    newFornecedor.save(err => {if (err) return err});
-
-    console.log(newFornecedor); //retirar em versão final
+  console.log(fornecedoresArr); //retirar em versão final
 });
--- public/index.js
+++ public/index.js
@@ -1,38 +1,32 @@
-var botaoCadastro = document.querySelector(".botaoCadastro");
-var popUpCadastro = document.querySelector(".jumbotron");
-var botaoVoltar = document.querySelector(".botaoVoltar");
-var formulario = document.querySelector(".formulario");
+let botaoCadastro = document.querySelector('.botaoCadastro');
+let popUpCadastro = document.querySelector('.jumbotron');
+let botaoVoltar = document.querySelector('.botaoVoltar');
+let formulario = document.querySelector('.formulario');
 
 function toggleVisibility() {
-    popUpCadastro.classList.toggle("hidden");
-    formulario.classList.toggle("hidden");
+  popUpCadastro.classList.toggle('hidden');
+  formulario.classList.toggle('hidden');
 }
 
-
-
 //funções de botao Jumbotron
-botaoCadastro.addEventListener("click", toggleVisibility);
-botaoVoltar.addEventListener("click", toggleVisibility);
-
+botaoCadastro.addEventListener('click', toggleVisibility);
+botaoVoltar.addEventListener('click', toggleVisibility);
 
 //VUE
 
-const fornecedoresArray = [];
-
 const app = new Vue({
-    el: '#fornecedores',
-    data: {
-        name: 'NOME FORNECEDOR',
-        ramo: "Ramo",
-        cidade: "Cidade",
-        telefone: "Telefone",
-        descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin malesuada ornare diam. Praesent suscipit diam id commodo tincidunt.",
-        imgAlim: "https://image.flaticon.com/icons/svg/2871/2871612.svg",
-        imgServ: "https://image.flaticon.com/icons/png/512/2304/2304375.png"
-    },
-    methods: {
-        filtrarCidade(cidade){
-            
-        }
-    }
-})
+  el: '#fornecedores',
+  data: {
+    name: 'NOME FORNECEDOR',
+    ramo: 'Ramo',
+    cidade: 'Cidade',
+    telefone: 'Telefone',
+    descricao:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin malesuada ornare diam. Praesent suscipit diam id commodo tincidunt.',
+    imgAlim: 'https://image.flaticon.com/icons/svg/2871/2871612.svg',
+    imgServ: 'https://image.flaticon.com/icons/png/512/2304/2304375.png',
+  },
+  methods: {
+    filtrarCidade(cidade) {},
+  },
+});
