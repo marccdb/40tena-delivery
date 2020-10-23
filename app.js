49c5557ec81759810054d920280f46ba50066501
Marcio Del Bianco <marcio.bianco@gmail.com>
2020-10-23 19:54:30 -0300

    style changes

--- app.js
+++ app.js
@@ -6,38 +6,41 @@ const mongoose = require("mongoose");
 app.use(express.static("public"));
 const schema = mongoose.Schema;
 app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
+	bodyParser.urlencoded({
+		extended: true,
+	})
 );
 
 //Set up connection
-const localPORT = 3000;
-app.listen(process.env.PORT || localPORT, () => {
-  console.log(`Listening on port ${localPORT}`);
+const LOCAL_PORT = 3000;
+app.listen(process.env.PORT || LOCAL_PORT, () => {
+	console.log(`Listening on port ${LOCAL_PORT}`);
 });
 
 //Mongoose (MongoDB) Set up
 
 function startServer() {
-  mongoose.connect(
-    "mongodb+srv://marcio:WfPtgB1NNVVENg9l@cluster0-rtrkq.gcp.mongodb.net/40tena-delivery?retryWrites=true&w=majority",
-    { useNewUrlParser: true, useUnifiedTopology: true },
-    (err) => {
-      if (err) console.log(err);
-      else console.log("MongoDB connection success");
-    }
-  );
+	let mongooseConnectionAddress =
+		"mongodb+srv://marcio:WfPtgB1NNVVENg9l@cluster0-rtrkq.gcp.mongodb.net/40tena-delivery?retryWrites=true&w=majority";
+
+	mongoose.connect(
+		mongooseConnectionAddress,
+		{ useNewUrlParser: true, useUnifiedTopology: true },
+		(err) => {
+			if (err) console.log(err);
+			else console.log("MongoDB connection success");
+		}
+	);
 }
 
 //SCHEMA
 
 const fornecedor_Schema = new mongoose.Schema({
-  name: String,
-  ramo: String,
-  cidade: String,
-  telefone: Number,
-  descricao: String,
+	name: String,
+	ramo: String,
+	cidade: String,
+	telefone: Number,
+	descricao: String,
 });
 
 //COLLECTION
@@ -45,50 +48,54 @@ const Fornecedor = mongoose.model("fornecedores", fornecedor_Schema);
 
 //ROUTES
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/view/index.html");
+	res.sendFile(__dirname + "/view/index.html");
 });
 
 app.post("/", (req, res) => {
-  let campoBusca = req.body.campoBusca;
+	let campoBusca = req.body.campoBusca;
 });
 
 app.post("/cadastro", (req, res) => {
-  let getName = req.body.formName;
-  let getCategory = req.body.formCategory;
-  let getCity = req.body.formCity;
-  let getTelephone = req.body.formTelephone;
-  let getDescription = req.body.formDescription;
+	let getName = req.body.formName;
+	let getCategory = req.body.formCategory;
+	let getCity = req.body.formCity;
+	let getTelephone = req.body.formTelephone;
+	let getDescription = req.body.formDescription;
 
-  startServer();
+	startServer();
 
-  const newFornecedor = new Fornecedor({
-    name: getName,
-    ramo: getCategory,
-    cidade: getCity,
-    telefone: getTelephone,
-    descricao: getDescription,
-  });
+	const newFornecedor = new Fornecedor({
+		name: getName,
+		category: getCategory,
+		city: getCity,
+		telephone: getTelephone,
+		description: getDescription,
+	});
 
-  newFornecedor.save((err) => {
-    if (err) console.log(err);
-  });
-
-  console.log(fornecedoresArr); //Remove from final version
+	newFornecedor.save((err) => {
+		if (err) console.log(err);
+	});
 });
 
 //Retrieve from database
 
 mongoose.connect(
-  "mongodb+srv://marcio:WfPtgB1NNVVENg9l@cluster0-rtrkq.gcp.mongodb.net/40tena-delivery?retryWrites=true&w=majority",
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  (err) => {
-    if (err) console.log(err);
-    else console.log("MongoDB connection success");
-  });
+	"mongodb+srv://marcio:WfPtgB1NNVVENg9l@cluster0-rtrkq.gcp.mongodb.net/40tena-delivery?retryWrites=true&w=majority",
+	{ useNewUrlParser: true, useUnifiedTopology: true },
+
+	(err) => {
+		if (err) console.log(err);
+		else console.log("MongoDB connection success");
+	}
+);
 
 Fornecedor.find((err, fornecedores) => {
-  if (err) console.log(err);
-  else console.log(fornecedores);
+	if (err) console.log(err);
+	else {
+		let fornecedorName = fornecedores[0].name;
+		let fornecedorType = fornecedores[0].category;
+		let fornecedorCity = fornecedores[0].city;
+		let fornecedorTelephone = fornecedores[0].telephone;
+		let fornecedorDescription = fornecedores[0].description;
+	}
 });
-
-module.exports = app;
--- public/index.js
+++ public/index.js
@@ -1,31 +1,36 @@
-const botaoCadastro = document.querySelector('.botaoCadastro');
-const popUpCadastro = document.querySelector('.jumbotron');
-const botaoVoltar = document.querySelector('.botaoVoltar');
-const formulario = document.querySelector('.formulario');
-
+const botaoCadastro = document.querySelector(".botaoCadastro");
+const popUpCadastro = document.querySelector(".jumbotron");
+const botaoVoltar = document.querySelector(".botaoVoltar");
+const formulario = document.querySelector(".formulario");
 
 function toggleVisibility() {
-  popUpCadastro.classList.toggle('hidden');
-  formulario.classList.toggle('hidden');
+	popUpCadastro.classList.toggle("hidden");
+	formulario.classList.toggle("hidden");
 }
 
 //Jumbotron actions
-botaoCadastro.addEventListener('click', toggleVisibility);
-botaoVoltar.addEventListener('click', toggleVisibility);
+botaoCadastro.addEventListener("click", toggleVisibility);
+botaoVoltar.addEventListener("click", toggleVisibility);
 
 //VUE
 
-const DISPLAY_NAME, DISPLAY_TYPE, DISPLAY_CITY, DISPLAY_PHONE, DISPLAY_DESCRIPTION;
+let DISPLAY_NAME = "Frutaria do José";
+let DISPLAY_CATEGORY = "Alimentação";
+let DISPLAY_CITY = "Vinhedo";
+let DISPLAY_PHONE = "(XX)1234-56789";
+let DISPLAY_DESCRIPTION = "Produtos frescos e entregas no horário comercial";
+let IMG_ALIM = "https://image.flaticon.com/icons/svg/2871/2871612.svg";
+let IMG_SERV = "https://image.flaticon.com/icons/png/512/2304/2304375.png";
 
-
-const app = new Vue({
-  el: '#fornecedores',
-  data: {
-    name: DISPLAY_NAME,
-    type: DISPLAY_TYPE,
-    city: DISPLAY_CITY,
-    phone: DISPLAY_PHONE,
-    descriptiopn: DISPLAY_DESCRIPTION,
-    imgAlim: 'https://image.flaticon.com/icons/svg/2871/2871612.svg',
-    imgServ: 'https://image.flaticon.com/icons/png/512/2304/2304375.png',
-  }});
+const vue = new Vue({
+	el: "#cardFornecedor",
+	data: {
+		name: DISPLAY_NAME,
+		category: DISPLAY_CATEGORY,
+		city: DISPLAY_CITY,
+		phone: DISPLAY_PHONE,
+		description: DISPLAY_DESCRIPTION,
+		imgAlim: IMG_ALIM,
+		imgServ: IMG_SERV,
+	},
+});
